52a5fdf7aef15cf1fe058ac7968f2ae9922f5b85
dennismoha <dennismoha2@gmail.com>
2025-09-12 12:04:34 +0300

    Added a dist file with pos changes

---
+++ frontend/dist/assets/index-DAesFtvV.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-Cziuzvc4.js";import{C as p}from"./chips.esm-Brcx-LcI.js";const C=({value:o,onChange:n,placeholder:r="Type and enter",className:s})=>e.jsx(p,{className:s,value:o,onChange:t=>n(t.value),placeholder:r});export{C};
