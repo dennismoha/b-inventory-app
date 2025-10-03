57404327447dbf20c42920ded154b01600806b1a
dennismoha <dennismoha2@gmail.com>
2025-10-03 12:07:45 +0300

    Added frontend dist files

---
+++ frontend/dist/assets/index-DLYdWQUM.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DsLUsQkV.js";import{C as p}from"./chips.esm-6h3bPeWW.js";const C=({value:o,onChange:n,placeholder:r="Type and enter",className:s})=>e.jsx(p,{className:s,value:o,onChange:t=>n(t.value),placeholder:r});export{C};
